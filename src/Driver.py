1bcd6c3d82fc18d3bf73d9110e691947ac96df76
Brighter-bits <85905618+Brighter-bits@users.noreply.github.com>
2023-05-14 19:08:53 +0100

    The GUI is now fully working

--- src/Driver.py
+++ src/Driver.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 option = webdriver.EdgeOptions()
-import time
 option.add_experimental_option("detach", True)
 option.add_argument("start-maximized")#The more space the easier it is to manipulate
 option.add_argument("inprivate")#Stops the 'log into edge' window from popping up AND gets rid of the sidebar
@@ -13,8 +12,9 @@ actions = ActionChains(driver)
 cookies = 0
 driver.implicitly_wait(20)
 driver.get('https://vle.mathswatch.co.uk')
-username = ""
-password = ""
+with open("Global Variables.txt", 'r') as f:
+    Login = f.readlines() # Open the Global Variables and store the two lines as a single list
+    f.close
 if cookies != 0:
     #Note: MathsWatch's cookies are short term, however the login window is absolute hell to navigate through.
     #This does however, bypass the login limit
@@ -24,11 +24,11 @@ if cookies != 0:
         driver.add_cookie(cookie)
 else:
     water = driver.find_element(by=By.XPATH, value='/html/body/modal/div[1]/div/div/div[2]/div/div[1]/form/forminput[1]/formgroup/div/div/span/input')
-    actions.click(water).send_keys_to_element(water, username).perform()
+    actions.click(water).send_keys_to_element(water, Login[0]).perform()
     water = driver.find_element(by=By.XPATH, value='/html/body/modal/div[1]/div/div/div[2]/div/div[1]/form/forminput[1]/formgroup/div/div/span/input')
-    actions.click(water).send_keys_to_element(water, username).perform()
+    actions.click(water).send_keys_to_element(water, Login[0]).perform()
     #Don't ask why it is done twice. IT JUST WORKS!
     water = driver.find_element(by=By.XPATH, value='/html/body/modal/div[1]/div/div/div[2]/div/div[1]/form/forminput[2]/formgroup/div/div/span/input')
-    actions.click(water).send_keys_to_element(water, password).perform()
+    actions.click(water).send_keys_to_element(water, Login[1]).perform()
     water = driver.find_element(by=By.XPATH, value='/html/body/modal/div[1]/div/div/div[2]/div/div[1]/form/div/div/div/div/button[2]')
     actions.click(water).perform()
--- src/GUI.py
+++ src/GUI.py
@@ -1,21 +1,67 @@
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLineEdit, QPushButton, QWidget
 
-app = QApplication([])
-window = QMainWindow()
-layout = QVBoxLayout()
 def DoPrimary():
+    #This is the function which will Start the bot, currently there is only Primary
+    with open('Global Variables.txt', 'w') as f:
+        #Stores the Username and Password in a plain text file (Kinda bad security, although I'm not sure how you could encrypt this)
+        #This means that Driver can take the Username and Password without importing GUI
+        #It also means that you won't have to reenter your username and password after restarting
+        f.write(UsernameBox.text())
+        f.write("\n")
+        f.write(PasswordBox.text())
+        f.close()
     import Main
-window.setWindowTitle("MathsWatch Bot")
-window.setGeometry(0, 0, 500, 500)
-# layout.addWidget(QLabel("Bot"), 0, 0)
-primarybutton = QPushButton("Start Primary questions")
-primarybutton.clicked.connect(DoPrimary)
-layout.addWidget(primarybutton)
-widget = QWidget()
-widget.setLayout(layout)
+    Main.Primary()
+
+def CreateWindow():
+    app = QApplication([])
+    window = QMainWindow()
+    layout = QVBoxLayout()
+    global UsernameBox
+    global PasswordBox
+    try:
+        with open('Global Variables.txt', 'r') as f:
+            Memory = f.readlines()
+            f.close()
+        
+        UsernameBox = QLineEdit()
+        UsernameBox.setText(Memory[0])
+        UsernameBox.setPlaceholderText("Username")
+
+        PasswordBox = QLineEdit()
+        PasswordBox.setText(Memory[1])
+        PasswordBox.setPlaceholderText("Password")
+        PasswordBox.setEchoMode(QLineEdit.EchoMode.Password)
+        
+    except:
+        UsernameBox = QLineEdit()
+        UsernameBox.setPlaceholderText("Username")
+
+        PasswordBox = QLineEdit()
+        PasswordBox.setPlaceholderText("Password")
+        PasswordBox.setEchoMode(QLineEdit.EchoMode.Password)
+        #Creates Username and Password Input boxes
+
+    window.setWindowTitle("MathsWatch Bot")
+    window.setGeometry(0, 0, 500, 500)
+    widgets = []
+    # layout.addWidget(QLabel("Bot"), 0, 0)
+    primarybutton = QPushButton("Start Primary questions")
+    primarybutton.clicked.connect(DoPrimary)
+
+    #Puts all of these values into a list
+    widgets.append(UsernameBox)
+    widgets.append(PasswordBox)
+    widgets.append(primarybutton)
 
 
-window.setMenuWidget(widget)
-window.show()
-app.exec()
+    for bob in range(len(widgets)):
+        layout.addWidget(widgets[bob])
+        #add each widget into the layout
+    widget = QWidget()
+    widget.setLayout(layout)
+
+    window.setMenuWidget(widget)
+    window.show()
+    app.exec()
+CreateWindow()
